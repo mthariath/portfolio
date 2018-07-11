b1174be4df1760d8aa320bcaed8a5133634b8e19
Michael Thariath <mictha1@gmail.com>
2018-07-11 00:07:29 -0500

    removed portfolio

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,8 @@ module.exports = {
   siteMetadata: {
     title: 'Michael Thariath',
   },
-  plugins: ['gatsby-plugin-glamor', 'gatsby-plugin-react-helmet', `gatsby-transformer-remark`,
-  {
-    resolve: `gatsby-source-filesystem`,
-    options: {
-      name: `portfolio`,
-      path: `${__dirname}/src/portfolio/`,
-    },
-  },]
+  plugins: [
+    'gatsby-plugin-glamor', 
+    'gatsby-plugin-react-helmet', 
+  ]
 }
