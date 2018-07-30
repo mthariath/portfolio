635743f0398da312d72035f82db42e3e1dfd708b
Michael Thariath <mictha1@gmail.com>
2018-07-30 13:24:15 -0500

    finished icon refactor

--- src/components/Elements/Icon.js
+++ src/components/Elements/Icon.js
@@ -1,70 +1,163 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import {Chip} from './Chip'
+import React from "react";
+import PropTypes from "prop-types";
+import { Chip } from "./Chip";
 
-const Icon = ({children, type}) => {
-    const icons = [
-        'icon-linux', 
-        'icon-trello', 
-        'icon-gulp', 
-        'icon-adobephotoshop', 
-        'icon-adobeindesign', 
-        'icon-adobeillustrator', 
-        'icon-googleanalytics', 
-        'icon-graphql', 
-        'icon-vuejs', 
-        'icon-webpack', 
-        'icon-python', 
-        'icon-git', 
-        'icon-github', 
-        'icon-react', 
-        'icon-sass', 
-        'icon-html5', 
-        'icon-css3', 
-        'icon-javascript', 
-        'icon-nodejs', 
-        'icon-terminal',
-        'icon-netlify',
-        'icon-heroku',
-        'icon-amazon',
-        'icon-docker',
-        'icon-stripe',
-        'icon-firebase',
-        'icon-mongodb',
-        'icon-php',
-        'icon-ruby',
-        'icon-npm',
-        'icon-gatsby'
-    ]
+const icons = [
+  {
+    text: "Linux",
+    code: "icon-linux"
+  },
+  {
+    text: "Trello",
+    code: "icon-trello"
+  },
+  {
+    text: "Gulp",
+    code: "icon-gulp"
+  },
+  {
+    text: "Photoshop",
+    code: "icon-adobephotoshop"
+  },
+  {
+    text: "InDesign",
+    code: "icon-adobeindesign"
+  },
+  {
+    text: "Analytics",
+    code: "icon-googleanalytics"
+  },
+  {
+    text: "Illustrator",
+    code: "icon-adobeillustrator"
+  },
+  {
+    text: "GraphQL",
+    code: "icon-graphql"
+  },
+  {
+    text: "VueJS",
+    code: "icon-vuejs"
+  },
+  {
+    text: "Webpack",
+    code: "icon-webpack"
+  },
+  {
+    text: "Python",
+    code: "icon-python"
+  },
+  {
+    text: "Git",
+    code: "icon-git"
+  },
+  {
+    text: "Github",
+    code: "icon-github"
+  },
+  {
+    text: "React",
+    code: "icon-react"
+  },
+  {
+    text: "SASS",
+    code: "icon-sass"
+  },
+  {
+    text: "HTML",
+    code: "icon-html5"
+  },
+  {
+    text: "CSS",
+    code: "icon-css3"
+  },
+  {
+    text: "JS",
+    code: "icon-javascript"
+  },
+  {
+    text: "NodeJS",
+    code: "icon-nodejs"
+  },
+  {
+    text: "CLI",
+    code: "icon-terminal"
+  },
+  {
+    text: "Netlify",
+    code: "icon-netlify"
+  },
+  {
+    text: "Heroku",
+    code: "icon-heroku"
+  },
+  {
+    text: "AWS",
+    code: "icon-amazon"
+  },
+  {
+    text: "Docker",
+    code: "icon-docker"
+  },
+  {
+    text: "Stripe",
+    code: "icon-stripe"
+  },
+  {
+    text: "Firebase",
+    code: "icon-firebase"
+  },
+  {
+    text: "MongoDB",
+    code: "icon-mongodb"
+  },
+  {
+    text: "PHP",
+    code: "icon-php"
+  },
+  {
+    text: "Ruby",
+    code: "icon-ruby"
+  },
+  {
+    text: "NPM",
+    code: "icon-npm"
+  },
+  {
+    text: "GatsbyJS",
+    code: "icon-gatsby"
+  }
+];
 
-    const iconClass = icons.find(icon=>(icon.includes(type)));
-    const wrapperCss = {
-        display: 'flex',
-        flexDirection: 'column',
-        fontSize: '3rem',
-        justifyContent: 'center',
-        alignItems: 'center',
-        transition: 'all 0.3s cubic-bezier(.82,.22,.44,1.52)',
-        '&:hover': {
-            transform: 'scale(1.05)'
-        },
-        '@media (max-width: 1024px)': {
-            fontSize: '2rem'
-        }
+const Icon = ({ small, caption, type }) => {
+  const iconClass = icons.find(icon => icon.code.includes(type)).code;
+  const text = icons.find(icon => icon.code.includes(type)).text;
+  const wrapperCss = {
+    display: "flex",
+    flexDirection: "column",
+    fontSize: small ? "2rem" : "3rem",
+    justifyContent: "center",
+    alignItems: "center",
+    transition: "all 0.3s cubic-bezier(.82,.22,.44,1.52)",
+    "&:hover": {
+      transform: "scale(1.05)"
+    },
+    "@media (max-width: 1024px)": {
+      fontSize: small ? "1.5rem" : "2rem"
     }
-    return (
-        <div css={wrapperCss}>
-            <span className={iconClass} />
-            {children && <Chip>{children}</Chip>}
-        </div>
-    )
-}
+  };
+
+  return (
+    <div css={wrapperCss}>
+      <span className={iconClass} />
+      {caption && <Chip>{text}</Chip>}
+    </div>
+  );
+};
 
 Icon.propTypes = {
-    children: PropTypes.node,
-    type: PropTypes.string.isRequired
-}
+  type: PropTypes.string.isRequired,
+  caption: PropTypes.bool
+};
 
-
-
-export {Icon}
+export { Icon };
