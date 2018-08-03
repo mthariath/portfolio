171b0d25cdb26775bd03c4ec465cfe393a833dee
Michael Thariath <mictha1@gmail.com>
2018-08-03 12:44:20 -0500

    fixed meta tags, added og

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -10,13 +10,20 @@ const Layout = ({ children, location }) => (
   <Page location={location.pathname}>
     <Helmet>
       <title>Mike Thariath</title>
-      <meta name="description" content="Sample" />
-      <meta name="keywords" content="sample, something" />
+      <meta name="description" content="The work of Mike Thariath, web developer and product designer." />
+      
       <meta
         name="google-site-verification"
         content="YB8HX2YUTpQXRI6OJAWGvv8XMVEFcdpTZ8s07TZdbo8"
       />
       <meta name="theme-color" content="#111" />
+      <meta property="og:url" content="https://thariath.com" />
+      <meta property="og:title" content="Mike Thariath" />
+      <meta property="og:description" content="The work of Mike Thariath, web developer and product designer." />
+      <meta property="og:image" content="http://www.thariath.com/og_image.png" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="627" />
+
       <link rel="shortcut icon" type="image/png" href={favicon} />
       <link
         rel="stylesheet"
