311ec315b0b5cbff9dfc20802aa1a71c9b988c04
Michael Thariath <mictha1@gmail.com>
2018-08-20 00:41:08 -0500

    made buttons accessible, removed unnecessary/duplicate ids

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -9,9 +9,13 @@ import "./Layout.css";
 const Layout = ({ children, location }) => (
   <Page location={location.pathname}>
     <Helmet>
+      <html lang="en" />
       <title>Mike Thariath</title>
-      <meta name="description" content="The work of Mike Thariath, web developer and product designer." />
-      
+      <meta
+        name="description"
+        content="The work of Mike Thariath, web developer and product designer."
+      />
+
       <meta
         name="google-site-verification"
         content="YB8HX2YUTpQXRI6OJAWGvv8XMVEFcdpTZ8s07TZdbo8"
@@ -19,8 +23,14 @@ const Layout = ({ children, location }) => (
       <meta name="theme-color" content="#111" />
       <meta property="og:url" content="https://thariath.com" />
       <meta property="og:title" content="Mike Thariath" />
-      <meta property="og:description" content="The work of Mike Thariath, web developer and product designer." />
-      <meta property="og:image" content="http://www.thariath.com/og_image.png" />
+      <meta
+        property="og:description"
+        content="The work of Mike Thariath, web developer and product designer."
+      />
+      <meta
+        property="og:image"
+        content="http://www.thariath.com/og_image.png"
+      />
       <meta property="og:image:width" content="1200" />
       <meta property="og:image:height" content="627" />
 
